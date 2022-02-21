cf83258288483694a1d42bc3af526e35a3d0bf78
thisLight <l1589002388@gmail.com>
2022-02-21 22:21:03 +0800

    promise: refresh interface and as_series() and flatmap()

--- away/promise.lua
+++ away/promise.lua
@@ -17,7 +17,11 @@
 
 local away = require "away"
 
+local promise_object_id = {}
+
 local promise_methods = {}
+
+promise_methods.object_id = promise_object_id
 
 local function do_wakeback_threads(threads)
     for i, v in ipairs(threads) do
@@ -26,6 +30,7 @@ local function do_wakeback_threads(threads)
 end
 
 function promise_methods:resolve(val)
+    assert(not self.fulfilled, "promise could not be fulfilled twice")
     self.fulfilled = true
     self.fulfilled_status = 'resolved'
     self.value = val
@@ -33,17 +38,18 @@ function promise_methods:resolve(val)
 end
 
 function promise_methods:reject(err)
+    assert(not self.fulfilled, "promise could not be fulfilled twice")
     self.fulfilled = true
     self.fulfilled_status ='rejected'
     self.error = err
     do_wakeback_threads(self.wakeback_threads)
 end
 
-function promise_methods:is_resovled()
+function promise_methods:resolvedp()
     return self.fulfilled_status == 'resolved'
 end
 
-function promise_methods:is_rejected()
+function promise_methods:rejectedp()
     return self.fulfilled_status == 'rejected'
 end
 
@@ -96,24 +102,51 @@ local function create(execfn)
     return new_t
 end
 
-function promise_methods:on_value(fn)
+local function is(obj)
+    return type(obj) == 'table' and obj.object_id == promise_object_id
+end
+
+function promise_methods:map(fn)
     return create(function(resolve)
         local value = self:wait()
         resolve((fn(value)))
     end)
 end
 
-function promise_methods:on_err(fn)
+function promise_methods:flatmap(fn)
+    return create(function (resolve, reject)
+        local value = self:wait()
+        if is(value) then
+            value:flatmap(fn):map(resolve):maperr(reject)
+        else
+            resolve((fn(value)))
+        end
+    end)
+end
+
+if pcall(require, "away.series") then
+    local Series = require "away.series"
+
+    function promise_methods:as_series()
+        local series = Series.create()
+        self:map(function (val)
+            series:put(val)
+        end)
+        return series
+    end
+end
+
+function promise_methods:maperr(fn)
     return create(function(resolve, reject)
         self:just_wait()
-        if self:is_rejected() then
+        if self:rejectedp() then
             local result = fn(self.error)
             if result ~= nil then
                 resolve(result)
             else
                 reject(self.error)
             end
-        elseif self:is_resovled() then
+        elseif self:resolvedp() then
             resolve(self.value)
         else
             error("this branch should not be reached")
@@ -137,13 +170,13 @@ local function race(t)
         local err
         local countdown = #t
         for i, v in ipairs(t) do
-            v:on_value(function(val)
+            v:map(function(val)
                 countdown = countdown - 1
                 if not promise.fullfilled then
                     resolve(val)
                 end
             end)
-            v:on_err(function(val)
+            v:maperr(function(val)
                 countdown = countdown - 1
                 err = val
                 if countdown <= 0 and not promise.fullfilled then
@@ -160,10 +193,12 @@ return {
     reject = promise_methods.reject,
     wait = promise_methods.wait,
     just_wait = promise_methods.just_wait,
-    on_value = promise_methods.on_value,
-    on_err = promise_methods.on_err,
+    map = promise_methods.map,
+    maperr = promise_methods.maperr,
     all = all,
     race = race,
-    is_rejected = promise_methods.is_rejected,
-    is_resovled = promise_methods.is_resovled,
+    rejectedp = promise_methods.rejectedp,
+    resolvedp = promise_methods.resolvedp,
+    is = is,
+    as_series = promise_methods.as_series,
 }
