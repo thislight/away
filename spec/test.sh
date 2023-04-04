f1b81307806d974e7b6d579e0a8cf9a2043fbff9
thisLight <l1589002388@gmail.com>
2023-04-04 17:29:44 +0800

    ci: fix project tree error

--- spec/test.sh
+++ spec/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /mnt/code || exit
+mkdir -p lua_modules
 if [ "$1" = "+valgrind" ]; then
     eval "$(luarocks path)" && luarocks install busted && luarocks build && valgrind --trace-children=yes --track-origins=yes --leak-check=full lua_modules/bin/busted
 else
