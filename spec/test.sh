665e6205c870cc8c5b98967955ffc1fabfb55d6e
thisLight <l1589002388@gmail.com>
2023-04-07 22:00:41 +0800

    test: building keep debug symbols

--- spec/test.sh
+++ spec/test.sh
@@ -2,7 +2,7 @@
 
 cd /mnt/code || exit
 if [ "$1" = "+valgrind" ]; then
-    eval "$(luarocks path)" && luarocks install busted && luarocks build && valgrind --trace-children=yes --track-origins=yes --leak-check=full luarocks test
+    eval "$(luarocks path)" && luarocks install busted && luarocks build CFLAGS="-O2 -fPIC -g" && valgrind --trace-children=yes --track-origins=yes --leak-check=full luarocks test
 else
-    eval "$(luarocks path)" && luarocks install busted && luarocks build && luarocks test
+    eval "$(luarocks path)" && luarocks install busted && luarocks build CFLAGS="-O2 -fPIC -g" && luarocks test
 fi
