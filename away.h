08de2d6e8dc1952921cfdab5309e44325b3a2a7d
thisLight <l1589002388@gmail.com>
2023-04-10 18:43:23 +0800

    away.h: added away_copycx and away_context

--- away.h
+++ away.h
@@ -43,6 +43,17 @@ struct away_sched {
 LUA_API struct away_sched *away_sched_new(lua_State *S, int nreg,
                                           away_Time timef, void *time_ud);
 
+/**
+ * @brief Spawn a new tracked thread. [-0, +1, e]
+ *
+ * This function does not apply any context on the new thread.
+ * Consider use `away_copycx` on it for the context,
+ * or the `away_context` and `away.context` will return `nil`.
+ *
+ * @param S 
+ * @param sched 
+ * @return lua_State* the new thread.
+ */
 LUA_API lua_State *away_spawn_thread(lua_State *S, struct away_sched *sched);
 
 LUA_API void away_sched_run(lua_State *S, struct away_sched *sched);
@@ -66,5 +77,23 @@ void away_yield(lua_State *S);
 void away_switchto(struct away_track *track, struct away_track *target);
 
 void away_pause(struct away_track *track);
+
+/**
+ * @brief Push the context table onto the stack, `nil` if no context for this thread. [-0, +1, m]
+ *
+ * @param track 
+ */
+void away_context(struct away_track *track);
+
+/**
+ * @brief Apply new context to `dst`, and push the context onto stack. [-0, +1, e]
+ *
+ * The context is a table, with a metatable. The metatable only has a `__index` set to another context.
+ * If `src` is not `NULL` and the `src` has context, the `__index` is the `src`'s context. Otherwise, it's the scheduler's root context.
+ * 
+ * @param src 
+ * @param dst 
+ */
+void away_copycx(struct away_track *src, struct away_track *dst);
 
 LUA_API int luaopen_away(lua_State *S);
